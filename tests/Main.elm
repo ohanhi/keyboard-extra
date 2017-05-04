df7cfcb1ce723844f41329c554544ddf364ab58f
ohanhi <ossi.hanhinen@gmail.com>
2017-05-04 22:06:53 +0300

    Only run doctests

--- tests/Main.elm
+++ tests/Main.elm
@@ -1,13 +1,13 @@
 port module Main exposing (..)
 
-import Tests
 import Test.Runner.Node exposing (run, TestProgram)
 import Json.Encode exposing (Value)
+import Doc.Tests
 
 
 main : TestProgram
 main =
-    run emit Tests.all
+    run emit Doc.Tests.all
 
 
 port emit : ( String, Value ) -> Cmd msg
